d32a0d88c38162a087eb77e465c2696b6d6a2ee3
ahahomecare <electricdoctor.vn@gmail.com>
2023-12-31 18:07:24 +0700

    hoangupd

--- ASP.Net_MVC_Core/Controllers/StudentController.cs
+++ ASP.Net_MVC_Core/Controllers/StudentController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using ASP.Net_MVC_Core.Models;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ASP.Net_MVC_Core.Controllers
@@ -16,6 +16,23 @@ namespace ASP.Net_MVC_Core.Controllers
             return View();
         }
         public IActionResult Info(int id)
+        {
+            var item = new Student(id);
+            return View(item);
+        }
+        public IActionResult class_student(int id)
+        {
+            return View();
+        }
+        public IActionResult course(int id)
+        {
+            return View();
+        }
+        public IActionResult point(int id)
+        {
+            return View();
+        }
+        public IActionResult edu_result(int id)
         {
             return View();
         }
