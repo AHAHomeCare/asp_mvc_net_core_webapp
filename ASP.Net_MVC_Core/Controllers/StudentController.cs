c7628b1b805170318ed73863b7ef0896db0cfe59
ahahomecare <electricdoctor.vn@gmail.com>
2023-12-31 17:51:59 +0700

    update bai tap TH02

---
+++ ASP.Net_MVC_Core/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASP.Net_MVC_Core.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Info(int id)
+        {
+            return View();
+        }
+    }
+}
+
