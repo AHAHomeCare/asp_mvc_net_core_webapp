96733e70285a00cc4e2d4a68e20bafdd5db0fe47
ahahomecare <electricdoctor.vn@gmail.com>
2023-12-31 12:08:44 +0700

    update bai TH02

---
+++ ASP.Net_MVC_Core/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ASP.Net_MVC_Core.Models
+{
+	public class Student
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public Nullable<DateTime> birthday { get; set; }
+		public int address_id { get; set; }
+		public string address_full { get; set; }
+
+		public Student()
+		{
+			this.id = 0;
+			this.name = string.Empty;
+			this.birthday = new DateTime(1900, 01, 01);
+			this.address_id = 0;
+			this.address_full = string.Empty;
+		}
+	}
+}
+
